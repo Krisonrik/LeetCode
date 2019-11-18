42c01d5387c753e25eb1d267c8118808d392b79d
Richard Yao <richard823@gmail.com>
2019-11-17 20:48:54 -0800

    Completed letter-combinations-of-a-phone-number

--- letter-combinations-of-a-phone-number.py
+++ letter-combinations-of-a-phone-number.py
@@ -1,16 +1,30 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        letter_char = {2: ['a', 'b', 'c'], 3: ['d', 'e', 'f'], 4: ['g', 'h', 'i'], 5: ['j', 'k', 'l'], 6: [
-            'm', 'n', 'o'], 7: ['p', 'q', 'r'], 8: ['t', 'u', 'v'], 9: ['w', 'x', 'y', 'z']}
+        letter_char = {0: [], 1: [], 2: ['a', 'b', 'c'], 3: ['d', 'e', 'f'], 4: ['g', 'h', 'i'], 5: ['j', 'k', 'l'], 6: [
+            'm', 'n', 'o'], 7: ['p', 'q', 'r', 's'], 8: ['t', 'u', 'v'], 9: ['w', 'x', 'y', 'z']}
+        if len(digits) == 0:
+            return []
         loops = 1
-        for i in digits:
-            loops *= len(letter_char[int(i)])
-        print(loops)
+        char_counter = []
+        char_pos_chart = []
         combinations = []
         for i in digits:
-            combinations.append
+            digit_val = int(i)
+            loops *= len(letter_char[digit_val])
+            char_counter.append(letter_char[digit_val])
+            char_pos_chart.append(1)
 
-        return []
+        # print(loops)
+        for i in range(loops):
+            cur_combo = ''
+            for j in range(len(char_pos_chart)):
+                if(char_pos_chart[j]-1 == len(char_counter[j])):
+                    char_pos_chart[j] = 1
+                    if j < len(char_counter)-1:
+                        char_pos_chart[j+1] += 1
+                cur_combo += char_counter[j][char_pos_chart[j]-1]
+            combinations.append(cur_combo)
+            # print(char_pos_chart)
+            char_pos_chart[0] += 1
 
-    def Increment(num: int) -> List[str]:
-        print(letter_char)
+        return combinations
